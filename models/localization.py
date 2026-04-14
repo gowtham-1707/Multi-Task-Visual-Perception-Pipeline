16f50025547c7aebe1db8f65e163909e7f902fc6
gowtham-1707 <ma24c051@smail.iitm.ac.in>
2026-04-14 15:20:21 +0530

    adding files

---
+++ models/localization.py
@@ -0,0 +1,29 @@
+
+import torch
+import torch.nn as nn
+from .vgg11 import VGG11Encoder
+from .layers import CustomDropout, SigmoidBBox
+
+
+class VGG11Localizer(nn.Module):
+    def __init__(self, in_channels: int = 3, dropout_p: float = 0.5):
+        super().__init__()
+        self.encoder = VGG11Encoder(in_channels=in_channels)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((7, 7))
+        self.regressor = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            CustomDropout(p=dropout_p),
+            nn.Linear(4096, 1024),
+            nn.ReLU(inplace=True),
+            CustomDropout(p=dropout_p),
+            nn.Linear(1024, 4),
+            nn.Sigmoid(),
+        )
+        self.image_size = 224
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.encoder(x, return_features=False)
+        x = self.adaptive_pool(x)
+        x = torch.flatten(x, 1)
+        return self.regressor(x) * self.image_size
